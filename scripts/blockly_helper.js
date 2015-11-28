b9f640bc64976528e5a8fb1f4505c69a02442a79
Nick Johnson <arachnid@notdot.net>
2015-11-28 10:50:52 +0000

    Removed obsolete and redundant code

--- scripts/blockly_helper.js
+++ scripts/blockly_helper.js
@@ -1,17 +1,4 @@
 /**
- * Execute the user's code.
- * Just a quick and dirty eval.  No checks for infinite loops, etc.
- */
-function runJS() {
-  var code = Blockly.Generator.workspaceToCode('JavaScript');
-  try {
-    eval(code);
-  } catch (e) {
-    alert('Program error:\n' + e);
-  }
-}
-
-/**
  * Backup code blocks to localStorage.
  */
 function backup_blocks() {
@@ -29,71 +16,6 @@ function restore_blocks() {
     var xml = Blockly.Xml.textToDom(window.localStorage.code);
     Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
   }
-}
-
-/**
-* Save Arduino generated code to local file.
-*/
-function saveCode() {
-  var fileName = window.prompt('What would you like to name your file?', 'BlocklyDuino')
-  //doesn't save if the user quits the save prompt
-  if(fileName){
-    var blob = new Blob([Blockly.Arduino.workspaceToCode()], {type: 'text/plain;charset=utf-8'});
-    saveAs(blob, fileName + '.ino');
-  }
-}
-
-/**
- * Save blocks to local file.
- * better include Blob and FileSaver for browser compatibility
- */
-function save() {
-  var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-  var data = Blockly.Xml.domToText(xml);
-  var fileName = window.prompt('What would you like to name your file?', 'BlocklyDuino');
-  // Store data in blob.
-  // var builder = new BlobBuilder();
-  // builder.append(data);
-  // saveAs(builder.getBlob('text/plain;charset=utf-8'), 'blockduino.xml');
-  if(fileName){
-    var blob = new Blob([data], {type: 'text/xml'});
-    saveAs(blob, fileName + ".xml");
-  } 
-}
-
-/**
- * Load blocks from local file.
- */
-function load(event) {
-  var files = event.target.files;
-  // Only allow uploading one file.
-  if (files.length != 1) {
-    return;
-  }
-
-  // FileReader
-  var reader = new FileReader();
-  reader.onloadend = function(event) {
-    var target = event.target;
-    // 2 == FileReader.DONE
-    if (target.readyState == 2) {
-      try {
-        var xml = Blockly.Xml.textToDom(target.result);
-      } catch (e) {
-        alert('Error parsing XML:\n' + e);
-        return;
-      }
-      var count = Blockly.mainWorkspace.getAllBlocks().length;
-      if (count && confirm('Replace existing blocks?\n"Cancel" will merge.')) {
-        Blockly.mainWorkspace.clear();
-      }
-      Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
-    }
-    // Reset value of input after loading because Chrome will not fire
-    // a 'change' event if the same file is loaded again.
-    document.getElementById('load').value = '';
-  };
-  reader.readAsText(files[0]);
 }
 
 /**
@@ -252,16 +174,6 @@ function uploadClick() {
             alert("Program uploaded ok");
         } else {
             alert("Error uploading program: " + errorInfo);
-        }
-    });
-}
-
-function resetClick() {
-    var code = "void setup() {} void loop() {}";
-
-    uploadCode(code, function(status, errorInfo) {
-        if (status != 200) {
-            alert("Error resetting program: " + errorInfo);
         }
     });
 }
